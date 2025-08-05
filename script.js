00fa8ef76020ec066a5da4c9a998aac426dc2961
Hudzaifah-Hanafie <hanafiyaahmad@gmail.com>
2025-08-05 14:40:16 +0700

    membuat fungsi isInvalidInput, fungsi addEntry untuk menambahkan entriy pada form dari dropdown, eventListener pada button, dan fungsi getCaloriesFromInputs untuk nantinya menghitung kalori dari inputan dari entry

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-const calorieCounter = document.getElementById('calorie-counter');
-const budgetNumberInput = document.getElementById('budget');
-const entryDropdown = document.getElementById('entry-dropdown');
-const addEntryButton = document.getElementById('add-entry');
-const clearButton = document.getElementById('clear');
-const output = document.getElementById('output');
+const calorieCounter = document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
 let isError = false;
 
 function cleanInputString(str) {
-    const regex = /[+-\s]/g;
-    return str.replace(regex, "");
+  const regex = /[+-\s]/g;
+  return str.replace(regex, "");
 }
+
+function isInvalidInput(str) {
+  const regex = /\d+e\d+/i;
+  return str.match(regex);
+}
+
+function addEntry() {
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+  const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name" />
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories" />`;
+  targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
+}
+
+function getCaloriesFromInputs(list) {
+    let calories = 0;
+    for (const item of list) {
+        const currVal = item.value;
+    }
+}
+
+addEntryButton.addEventListener("click", addEntry);
